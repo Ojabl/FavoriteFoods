439820c4b0d4476bfc52a85568c164a4cc9cbbfc
osajab10@gmail.com <osajab10@gmail.com>
2022-09-16 16:43:28 +0200

    Add project files.

---
+++ FavoriteFoods/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FavoriteFoods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> favoriteFoods = new Dictionary<string, string>();
+            favoriteFoods["Alex"] = "hot dogs";
+            favoriteFoods["A'ja"] = "pizza";
+            favoriteFoods["Jules"] = "falafel";
+            favoriteFoods["Naima"] = "spaghetti";
+
+            string name;
+            while ((name = Console.ReadLine()) != "")
+            {
+                if (favoriteFoods.ContainsKey(name))
+                    Console.WriteLine($"{name}'s favorite food is {favoriteFoods[name]}");
+                else
+                    Console.WriteLine($"I don't know {name}'s favorite food");
+            }
+        }
+    }
+}
